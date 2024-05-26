4f2ccd16b0ea3f2ceb4dcf8f6548ca01c1d18450
Narongchai (lmDOW) <bem255541@gmail.com>
2024-05-27 04:30:03 +0700

    add calendar component + adjust layout

--- src/App.jsx
+++ src/App.jsx
@@ -2,22 +2,61 @@ import { FaHome } from "react-icons/fa";
 import { GiHamburgerMenu } from "react-icons/gi";
 import { FaCalendarDays } from "react-icons/fa6";
 import { RiTodoFill } from "react-icons/ri";
+import Calendar from 'react-calendar'
+import 'react-calendar/dist/Calendar.css';
 function App() {
 
   return (
     <>
       <section className="content">
-      <div className=" drawer lg:drawer-open ">
+      <div className="drawer lg:drawer-open ">
           <input id="my-drawer-2" type="checkbox" className="drawer-toggle" />
-          <div className="drawer-content bg-red-200/25">
+          <div className="drawer-content bg-red-400/75">
               <div className="main-menu">
-                <div className="max-w-screen-xl ">
-                  <div className="nav-logo flex items-center text-lg">
+                <div className="max-w-screen p-10">
+                    <div className="flex justify-center  gap-20">
+                      <div className="bg-white w-[50%] h-[385px] shadow  rounded-lg p-10">
                         <label htmlFor="my-drawer-2" className="drawer-button me-5 lg:hidden">
                           <GiHamburgerMenu />
                         </label>
-                       
-                        <p className="font-bold ms-2 hidden md:block">Hometest</p>
+                        <Calendar />
+                      </div>
+                      <div className="bg-white w-[25%] h-[385px] group overflow-auto  shadow  rounded-lg p-10">
+                        <div className="flex items-center justify-between">
+                          <p className="text-lg font-bold">Daily Tasks</p>
+                          <p className="text-sm font-bold text-red-400/75">2 tasks</p>
+                        </div>
+                          <div className="task-group p-2 flex justify-between my-4 items-center rounded-full bg-red-300/25 w-full">
+                            <p className="font-bold ms-2">อาบน้ำ</p>
+                              <div className="check w-8 h-8 bg-red-300 rounded-full">
+                              </div>
+                          </div>
+                          <div className="task-group p-2 flex justify-between my-4 items-center rounded-full bg-red-300/25 w-full">
+                            <p className="font-bold ms-2">อาบน้ำ</p>
+                              <div className="check w-8 h-8 bg-red-300 rounded-full">
+                              </div>
+                          </div>
+                          <div className="task-group p-2 flex justify-between my-4 items-center rounded-full bg-red-300/25 w-full">
+                            <p className="font-bold ms-2">อาบน้ำ</p>
+                              <div className="check w-8 h-8 bg-red-300 rounded-full">
+                              </div>
+                          </div>
+                          <div className="task-group p-2 flex justify-between my-4 items-center rounded-full bg-red-300/25 w-full">
+                            <p className="font-bold ms-2">อาบน้ำ</p>
+                              <div className="check w-8 h-8 bg-red-300 rounded-full">
+                              </div>
+                          </div>
+                          <div className="task-group p-2 flex justify-between my-4 items-center rounded-full bg-red-300/25 w-full">
+                            <p className="font-bold ms-2">อาบน้ำ</p>
+                              <div className="check w-8 h-8 bg-red-300 rounded-full">
+                              </div>
+                          </div>
+                          <div className="task-group p-2 flex justify-between my-4 items-center rounded-full bg-red-300/25 w-full">
+                            <p className="font-bold ms-2">อาบน้ำ</p>
+                              <div className="check w-8 h-8 bg-red-300 rounded-full">
+                              </div>
+                          </div>
+                      </div>
                     </div>
                 </div>
               </div>
