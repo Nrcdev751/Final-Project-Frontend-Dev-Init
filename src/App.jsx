0cae811cefcc8fdb15222584c2e56c9e75fad961
Narongchai (lmDOW) <bem255541@gmail.com>
2024-05-29 03:49:01 +0700

    add logo + responsive

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,8 @@ import { FaCalendarDays } from "react-icons/fa6";
 import { RiTodoFill } from "react-icons/ri";
 import Calendar from 'react-calendar'
 import 'react-calendar/dist/Calendar.css';
+import logo from './assets/logo.svg'
+
 function App() {
 
   return (
@@ -14,14 +16,15 @@ function App() {
           <div className="drawer-content bg-red-400/75">
               <div className="main-menu">
                 <div className="max-w-screen p-10">
-                    <div className="flex justify-center  gap-20">
-                      <div className="bg-white w-[50%] h-[385px] shadow  rounded-lg p-10">
-                        <label htmlFor="my-drawer-2" className="drawer-button me-5 lg:hidden">
-                          <GiHamburgerMenu />
+                        <label htmlFor="my-drawer-2" className="drawer-button me-5  lg:hidden">
+                          <GiHamburgerMenu  className="text-white text-lg"/>
                         </label>
+                    <div className="flex justify-center flex-col md:flex-row  gap-20">
+                    <img src={logo} className="md:hidden filter brightness-0 invert  scale-75" alt="Website Logo" />
+                      <div className="bg-white w-100 md:w-[50%] h-[385px] shadow  rounded-lg p-10">
                         <Calendar />
                       </div>
-                      <div className="bg-white w-[25%] h-[385px] group overflow-auto  shadow  rounded-lg p-10">
+                      <div className="bg-white w-100 md:w-[25%] h-[385px] group overflow-auto  shadow  rounded-lg p-10">
                         <div className="flex items-center justify-between">
                           <p className="text-lg font-bold">Daily Tasks</p>
                           <p className="text-sm font-bold text-red-400/75">2 tasks</p>
@@ -64,6 +67,7 @@ function App() {
           <div className="drawer-side shadow-md">
             <label htmlFor="my-drawer-2" aria-label="close sidebar" className="drawer-overlay"></label> 
             <ul className="menu p-4 w-80 min-h-full bg-white text-base-content">
+                <img src={logo} alt="Website Logo" />
                 <div className="ms-5 flex my-3 w-[250px] text-lg items-center bg-white rounded-md p-2  shadow-md">
                   <FaHome className="text-red-500/75"/>
                   <li className="font-semibold ms-2">Overview</li>
